41f8ce50c6e4afe7cec1c6b3afd7dce4d46939cc
Tim Horton <tmhorton@gmail.com>
2014-07-25 14:36:17 -0700

    Removed tabs

--- AirBitz/Login/LoginViewController.m
+++ AirBitz/Login/LoginViewController.m
@@ -17,27 +17,27 @@
 #import "PasswordRecoveryViewController.h"
 #import "CoreBridge.h"
 
-#define CONTENT_VIEW_SCALE_WITH_KEYBOARD	0.75
-#define LOGO_IMAGE_SHRINK_SCALE_FACTOR		0.5
+#define CONTENT_VIEW_SCALE_WITH_KEYBOARD    0.75
+#define LOGO_IMAGE_SHRINK_SCALE_FACTOR        0.5
 
 typedef enum eLoginMode
 {
-	MODE_ENTERING_NEITHER,
-	MODE_ENTERING_USERNAME,
-	MODE_ENTERING_PASSWORD
+    MODE_ENTERING_NEITHER,
+    MODE_ENTERING_USERNAME,
+    MODE_ENTERING_PASSWORD
 } tLoginMode;
 
 @interface LoginViewController () <UITextFieldDelegate, SignUpViewControllerDelegate, PasswordRecoveryViewControllerDelegate>
 {
-	tLoginMode                      _mode;
-	CGRect                          _originalContentFrame;
-	CGRect                          _originalLogoFrame;
-	CGRect                          _originalSwipeArrowFrame;
-	CGPoint                         _firstTouchPoint;
-	BOOL                            _bSuccess;
-	NSString                        *_strReason;
-	SignUpViewController            *_signUpController;
-	UITextField                     *_activeTextField;
+    tLoginMode                      _mode;
+    CGRect                          _originalContentFrame;
+    CGRect                          _originalLogoFrame;
+    CGRect                          _originalSwipeArrowFrame;
+    CGPoint                         _firstTouchPoint;
+    BOOL                            _bSuccess;
+    NSString                        *_strReason;
+    SignUpViewController            *_signUpController;
+    UITextField                     *_activeTextField;
     PasswordRecoveryViewController  *_passwordRecoveryController;
 }
 @property (nonatomic, weak) IBOutlet UIView             *contentView;
@@ -67,23 +67,23 @@ typedef enum eLoginMode
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
-	_mode = MODE_ENTERING_NEITHER;
-	_originalContentFrame = self.contentView.frame;
-	_originalLogoFrame = self.logoImage.frame;
-	_originalSwipeArrowFrame = self.swipeArrow.frame;
-	
-	self.userNameTextField.delegate = self;
-	self.passwordTextField.delegate = self;
-	self.invalidMessage.hidden = YES;
+    // Do any additional setup after loading the view.
+    _mode = MODE_ENTERING_NEITHER;
+    _originalContentFrame = self.contentView.frame;
+    _originalLogoFrame = self.logoImage.frame;
+    _originalSwipeArrowFrame = self.swipeArrow.frame;
+    
+    self.userNameTextField.delegate = self;
+    self.passwordTextField.delegate = self;
+    self.invalidMessage.hidden = YES;
     self.spinnerView.hidden = YES;
-	
-	#if HARD_CODED_LOGIN
-	
-	self.userNameTextField.text = HARD_CODED_LOGIN_NAME;
-	self.passwordTextField.text = HARD_CODED_LOGIN_PASSWORD;
-	#endif
-	
+    
+    #if HARD_CODED_LOGIN
+    
+    self.userNameTextField.text = HARD_CODED_LOGIN_NAME;
+    self.passwordTextField.text = HARD_CODED_LOGIN_PASSWORD;
+    #endif
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -116,9 +116,9 @@ typedef enum eLoginMode
 
 - (IBAction)SignIn
 {
-	[self.userNameTextField resignFirstResponder];
-	[self.passwordTextField resignFirstResponder];
-	[self animateToInitialPresentation];
+    [self.userNameTextField resignFirstResponder];
+    [self.passwordTextField resignFirstResponder];
+    [self animateToInitialPresentation];
 
     _bSuccess = NO;
     tABC_Error Error;
@@ -130,49 +130,49 @@ typedef enum eLoginMode
     if (Error.code != ABC_CC_Ok)
     {
         [Util printABC_Error:&Error];
-		self.invalidMessage.hidden = NO;
+        self.invalidMessage.hidden = NO;
     }
     else
     {
         [self showSpinner:YES];
-		self.invalidMessage.hidden = YES;
+        self.invalidMessage.hidden = YES;
     }
 }
 
 - (IBAction)SignUp
 {
-	[self.userNameTextField resignFirstResponder];
-	[self.passwordTextField resignFirstResponder];
-	[self animateToInitialPresentation];
-	
-	UIStoryboard *mainStoryboard = [UIStoryboard storyboardWithName:@"Main_iPhone" bundle: nil];
-	_signUpController = [mainStoryboard instantiateViewControllerWithIdentifier:@"SignUpViewController"];
+    [self.userNameTextField resignFirstResponder];
+    [self.passwordTextField resignFirstResponder];
+    [self animateToInitialPresentation];
+    
+    UIStoryboard *mainStoryboard = [UIStoryboard storyboardWithName:@"Main_iPhone" bundle: nil];
+    _signUpController = [mainStoryboard instantiateViewControllerWithIdentifier:@"SignUpViewController"];
 
     _signUpController.mode = SignUpMode_SignUp;
-	_signUpController.delegate = self;
-	
-	CGRect frame = self.view.bounds;
-	frame.origin.x = frame.size.width;
-	_signUpController.view.frame = frame;
-	[self.view addSubview:_signUpController.view];
-	
-	
-	[UIView animateWithDuration:0.35
-						  delay:0.0
-						options:UIViewAnimationOptionCurveEaseInOut
-					 animations:^
-	 {
-		 _signUpController.view.frame = self.view.bounds;
-	 }
-	 completion:^(BOOL finished)
-	 {
-	 }];
+    _signUpController.delegate = self;
+    
+    CGRect frame = self.view.bounds;
+    frame.origin.x = frame.size.width;
+    _signUpController.view.frame = frame;
+    [self.view addSubview:_signUpController.view];
+    
+    
+    [UIView animateWithDuration:0.35
+                          delay:0.0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^
+     {
+         _signUpController.view.frame = self.view.bounds;
+     }
+     completion:^(BOOL finished)
+     {
+     }];
 }
 
 - (IBAction)buttonForgotTouched:(id)sender
 {
     [self.userNameTextField resignFirstResponder];
-	[self.passwordTextField resignFirstResponder];
+    [self.passwordTextField resignFirstResponder];
 
     // if they have a username
     if ([self.userNameTextField.text length])
@@ -251,44 +251,44 @@ typedef enum eLoginMode
 
 - (void)animateSwipeArrowWithRepetitions:(int)repetitions andDelay:(float)delay;
 {
-	static int repetitionCount;
-	
-	if(repetitions)
-	{
-		repetitionCount = repetitions;
-	}
-	[UIView animateWithDuration:0.35
-						  delay:delay
-						options:UIViewAnimationOptionCurveEaseInOut
-					 animations:^
-	 {
-		 CGRect frame = self.swipeArrow.frame;
-		 frame.origin.x = _originalSwipeArrowFrame.origin.x - _originalSwipeArrowFrame.size.width;
-		 self.swipeArrow.frame = frame;
-		 
-	 }
-	 completion:^(BOOL finished)
-	 {
-		 [UIView animateWithDuration:0.45
-							   delay:0.0
-							 options:UIViewAnimationOptionCurveEaseInOut
-						  animations:^
-		  {
-			  CGRect frame = self.swipeArrow.frame;
-			  frame.origin.x = _originalSwipeArrowFrame.origin.x;
-			  self.swipeArrow.frame = frame;
-			  
-		  }
-						  completion:^(BOOL finished)
-		  {
-			  //self.dividerView.alpha = 0.0;
-			  repetitionCount--;
-			  if(repetitionCount)
-			  {
-				  [self animateSwipeArrowWithRepetitions:0 andDelay:0];
-			  }
-		  }];
-	 }];
+    static int repetitionCount;
+    
+    if(repetitions)
+    {
+        repetitionCount = repetitions;
+    }
+    [UIView animateWithDuration:0.35
+                          delay:delay
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^
+     {
+         CGRect frame = self.swipeArrow.frame;
+         frame.origin.x = _originalSwipeArrowFrame.origin.x - _originalSwipeArrowFrame.size.width;
+         self.swipeArrow.frame = frame;
+         
+     }
+     completion:^(BOOL finished)
+     {
+         [UIView animateWithDuration:0.45
+                               delay:0.0
+                             options:UIViewAnimationOptionCurveEaseInOut
+                          animations:^
+          {
+              CGRect frame = self.swipeArrow.frame;
+              frame.origin.x = _originalSwipeArrowFrame.origin.x;
+              self.swipeArrow.frame = frame;
+              
+          }
+                          completion:^(BOOL finished)
+          {
+              //self.dividerView.alpha = 0.0;
+              repetitionCount--;
+              if(repetitionCount)
+              {
+                  [self animateSwipeArrowWithRepetitions:0 andDelay:0];
+              }
+          }];
+     }];
 }
 
 - (CGFloat)StatusBarHeight
@@ -299,116 +299,116 @@ typedef enum eLoginMode
 
 - (void)animateToInitialPresentation
 {
-	[UIView animateWithDuration:0.35
-						  delay: 0.0
-						options: UIViewAnimationOptionCurveEaseInOut
-					 animations:^
-	 {
-		 self.contentView.frame = _originalContentFrame;
-		 
-		 self.swipeArrow.alpha = 1.0;
-		 self.swipeText.alpha = 1.0;
-		 self.titleText.alpha = 1.0;
-		 
-		 self.logoImage.transform = CGAffineTransformMakeScale(1.0, 1.0);
-		 self.logoImage.frame = _originalLogoFrame;
-		 self.logoImage.alpha = 1.0;
-	 }
-					 completion:^(BOOL finished)
-	 {
-		 _mode = MODE_ENTERING_NEITHER;
-	 }];
+    [UIView animateWithDuration:0.35
+                          delay: 0.0
+                        options: UIViewAnimationOptionCurveEaseInOut
+                     animations:^
+     {
+         self.contentView.frame = _originalContentFrame;
+         
+         self.swipeArrow.alpha = 1.0;
+         self.swipeText.alpha = 1.0;
+         self.titleText.alpha = 1.0;
+         
+         self.logoImage.transform = CGAffineTransformMakeScale(1.0, 1.0);
+         self.logoImage.frame = _originalLogoFrame;
+         self.logoImage.alpha = 1.0;
+     }
+                     completion:^(BOOL finished)
+     {
+         _mode = MODE_ENTERING_NEITHER;
+     }];
 }
 
 #pragma mark - keyboard callbacks
 
 - (void)keyboardWillShow:(NSNotification *)notification
 {
-	BOOL shrinkLogo = NO;
-	//Get KeyboardFrame (in Window coordinates)
-	if(_activeTextField)
-	{
-		NSDictionary *userInfo = [notification userInfo];
-		CGRect keyboardFrame = [[userInfo objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
-		
-		CGRect ownFrame = [self.view.window convertRect:keyboardFrame toView:self.view];
-		
-		float remainingSpace = ownFrame.origin.y - [self StatusBarHeight];
-		
-		remainingSpace -= self.userEntryView.frame.size.height;
-		
-		float logoScaleFactor = remainingSpace / self.logoImage.frame.size.height;
-		if(logoScaleFactor >= LOGO_IMAGE_SHRINK_SCALE_FACTOR)
-		{
-			shrinkLogo = YES;
-		}
-		[UIView animateWithDuration:0.35
-							  delay: 0.0
-							options: UIViewAnimationOptionCurveEaseInOut
-						 animations:^
-		 {
-			 CGRect frame = self.contentView.frame;
-			 
-			 
-			 self.swipeArrow.alpha = 0.0;
-			 self.swipeText.alpha = 0.0;
-			 self.titleText.alpha = 0.0;
-			 
-			 if(shrinkLogo)
-			 {
-				 frame.origin.y = 22.0 + _originalLogoFrame.size.height * logoScaleFactor;
-				 frame.size.height = keyboardFrame.origin.y - frame.origin.y;
-				 self.contentView.frame = frame;
-				 
-				 self.logoImage.transform = CGAffineTransformMakeScale(logoScaleFactor, logoScaleFactor);
-				 frame = self.logoImage.frame;
-				 frame.origin.y = 22.0;
-				 self.logoImage.frame = frame;
-			}
-			else
-			{
-				frame.origin.y = [self StatusBarHeight];
-				frame.size.height = keyboardFrame.origin.y - frame.origin.y;
-				self.contentView.frame = frame;
+    BOOL shrinkLogo = NO;
+    //Get KeyboardFrame (in Window coordinates)
+    if(_activeTextField)
+    {
+        NSDictionary *userInfo = [notification userInfo];
+        CGRect keyboardFrame = [[userInfo objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+        
+        CGRect ownFrame = [self.view.window convertRect:keyboardFrame toView:self.view];
+        
+        float remainingSpace = ownFrame.origin.y - [self StatusBarHeight];
+        
+        remainingSpace -= self.userEntryView.frame.size.height;
+        
+        float logoScaleFactor = remainingSpace / self.logoImage.frame.size.height;
+        if(logoScaleFactor >= LOGO_IMAGE_SHRINK_SCALE_FACTOR)
+        {
+            shrinkLogo = YES;
+        }
+        [UIView animateWithDuration:0.35
+                              delay: 0.0
+                            options: UIViewAnimationOptionCurveEaseInOut
+                         animations:^
+         {
+             CGRect frame = self.contentView.frame;
+             
+             
+             self.swipeArrow.alpha = 0.0;
+             self.swipeText.alpha = 0.0;
+             self.titleText.alpha = 0.0;
+             
+             if(shrinkLogo)
+             {
+                 frame.origin.y = 22.0 + _originalLogoFrame.size.height * logoScaleFactor;
+                 frame.size.height = keyboardFrame.origin.y - frame.origin.y;
+                 self.contentView.frame = frame;
+                 
+                 self.logoImage.transform = CGAffineTransformMakeScale(logoScaleFactor, logoScaleFactor);
+                 frame = self.logoImage.frame;
+                 frame.origin.y = 22.0;
+                 self.logoImage.frame = frame;
+            }
+            else
+            {
+                frame.origin.y = [self StatusBarHeight];
+                frame.size.height = keyboardFrame.origin.y - frame.origin.y;
+                self.contentView.frame = frame;
 
-				self.logoImage.alpha = 0.0;
-			}
-		 }
-						 completion:^(BOOL finished)
-		 {
-			 
-		 }];
-	}
+                self.logoImage.alpha = 0.0;
+            }
+         }
+                         completion:^(BOOL finished)
+         {
+             
+         }];
+    }
 }
 
 - (void)keyboardWillHide:(NSNotification *)notification
 {
-	if(_activeTextField)
-	{
-		 _activeTextField = nil;
-	}
+    if(_activeTextField)
+    {
+         _activeTextField = nil;
+    }
 }
 
 #pragma mark - touch events (for swiping)
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-	UITouch *touch = [touches anyObject];
-	_firstTouchPoint = [touch locationInView:self.view.window];
+    UITouch *touch = [touches anyObject];
+    _firstTouchPoint = [touch locationInView:self.view.window];
 }
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
-	UITouch *touch = [touches anyObject];
-	CGPoint touchPoint = [touch locationInView:self.view.window];
-	
-	CGRect frame = self.view.frame;
-	CGFloat xPos;
-	
-	xPos = touchPoint.x - _firstTouchPoint.x;
-	
-	frame.origin.x = xPos;
-	self.view.frame = frame;
+    UITouch *touch = [touches anyObject];
+    CGPoint touchPoint = [touch locationInView:self.view.window];
+    
+    CGRect frame = self.view.frame;
+    CGFloat xPos;
+    
+    xPos = touchPoint.x - _firstTouchPoint.x;
+    
+    frame.origin.x = xPos;
+    self.view.frame = frame;
 }
 
 - (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
@@ -417,70 +417,70 @@ typedef enum eLoginMode
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
-	float xOffset = self.view.frame.origin.x;
-	if(xOffset < 0) xOffset = -xOffset;
-	if(xOffset < self.view.frame.size.width / 2)
-	{
-		//spring back
-		[UIView animateWithDuration:0.35
-							  delay:0.0
-							options:UIViewAnimationOptionCurveEaseInOut
-						 animations:^
-		 {
-			 CGRect frame = self.view.frame;
-			frame.origin.x = 0.0;
-			 self.view.frame = frame;
-		 }
-		completion:^(BOOL finished)
-		 {
-		 }];
-	}
-	else
-	{
-		//spring out
-		[UIView animateWithDuration:0.35
-							  delay:0.0
-							options:UIViewAnimationOptionCurveEaseInOut
-						 animations:^
-		 {
-			 CGRect frame = self.view.frame;
-			 if(frame.origin.x < 0)
-			 {
-				 frame.origin.x = -frame.size.width;
-			 }
-			 else
-			 {
-				 frame.origin.x = frame.size.width;
-			 }
-			 self.view.frame = frame;
-		 }
-		 completion:^(BOOL finished)
-		 {
-			 self.invalidMessage.hidden = YES;
-			 [self.delegate loginViewControllerDidAbort];
-		 }];
-	}
+    float xOffset = self.view.frame.origin.x;
+    if(xOffset < 0) xOffset = -xOffset;
+    if(xOffset < self.view.frame.size.width / 2)
+    {
+        //spring back
+        [UIView animateWithDuration:0.35
+                              delay:0.0
+                            options:UIViewAnimationOptionCurveEaseInOut
+                         animations:^
+         {
+             CGRect frame = self.view.frame;
+            frame.origin.x = 0.0;
+             self.view.frame = frame;
+         }
+        completion:^(BOOL finished)
+         {
+         }];
+    }
+    else
+    {
+        //spring out
+        [UIView animateWithDuration:0.35
+                              delay:0.0
+                            options:UIViewAnimationOptionCurveEaseInOut
+                         animations:^
+         {
+             CGRect frame = self.view.frame;
+             if(frame.origin.x < 0)
+             {
+                 frame.origin.x = -frame.size.width;
+             }
+             else
+             {
+                 frame.origin.x = frame.size.width;
+             }
+             self.view.frame = frame;
+         }
+         completion:^(BOOL finished)
+         {
+             self.invalidMessage.hidden = YES;
+             [self.delegate loginViewControllerDidAbort];
+         }];
+    }
 }
 
 #pragma mark - UITextField delegates
 
 - (void)textFieldDidBeginEditing:(UITextField *)textField
 {
-	//called when user taps on either search textField or location textField
-	self.invalidMessage.hidden = YES;
-	_activeTextField = textField;
-	
-	if(_mode == MODE_ENTERING_NEITHER)
-	{
-		if(textField == self.userNameTextField)
-		{
-			_mode = MODE_ENTERING_USERNAME;
-		}
-		else
-		{
-			_mode = MODE_ENTERING_PASSWORD;
-		}
-	}
+    //called when user taps on either search textField or location textField
+    self.invalidMessage.hidden = YES;
+    _activeTextField = textField;
+    
+    if(_mode == MODE_ENTERING_NEITHER)
+    {
+        if(textField == self.userNameTextField)
+        {
+            _mode = MODE_ENTERING_USERNAME;
+        }
+        else
+        {
+            _mode = MODE_ENTERING_PASSWORD;
+        }
+    }
 
     // highlight all of the text
     if (textField == self.userNameTextField)
@@ -503,7 +503,7 @@ typedef enum eLoginMode
         [self animateToInitialPresentation];
     }
 
-	return NO;
+    return NO;
 }
 
 - (void)signInComplete
@@ -519,7 +519,7 @@ typedef enum eLoginMode
     }
     else
     {
-		self.invalidMessage.hidden = NO;
+        self.invalidMessage.hidden = NO;
 
         [User Singleton].name = nil;
         [User Singleton].password = nil; 
