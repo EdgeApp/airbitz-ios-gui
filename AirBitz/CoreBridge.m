18d31a5008ab1781625472a70ff6a28d9663b9e7
Tim Horton <tim@airbitz.co>
2014-12-23 11:26:25 -0800

    Fixed pin login after an autologout

--- AirBitz/CoreBridge.m
+++ AirBitz/CoreBridge.m
@@ -1054,6 +1054,7 @@ static NSTimer *_notificationTimer;
 {
     [CoreBridge stopWatchers];
     [CoreBridge stopQueues];
+    [LocalSettings saveAll];
 
     tABC_Error Error;
     tABC_CC result = ABC_ClearKeyCache(&Error);
