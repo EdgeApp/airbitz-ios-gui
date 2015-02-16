ffed6355fba5312bc48f93c232076e67ce10cbe4
Paul Puey <paul@airbitz.co>
2015-02-16 13:04:37 -0800

    Add StylizedTextField2 used for bizdir Add macro to set color by hex value

---
+++ AirBitz/StylizedTextField2.h
@@ -0,0 +1,15 @@
+//
+//  StylizedTextField.h
+//  AirBitz
+//
+//  Created by Carson Whitsett on 4/5/14.
+//  Copyright (c) 2014 AirBitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StylizedTextField2 : UITextField
+
+- (void)resetBorder;
+
+@end
--- AirBitz/Util.h
+++ AirBitz/Util.h
@@ -10,6 +10,19 @@
 #import <AddressBookUI/AddressBookUI.h>
 #import "ABC.h"
 
+#define UIColorFromRGB(rgbValue) \
+        [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+                        green:((float)((rgbValue & 0x00FF00) >>  8))/255.0 \
+                         blue:((float)((rgbValue & 0x0000FF) >>  0))/255.0 \
+                        alpha:1.0]
+
+#define UIColorFromARGB(rgbValue) \
+        [UIColor colorWithRed:((float)((rgbValue & 0x00FF0000) >> 16))/255.0 \
+                        green:((float)((rgbValue & 0x0000FF00) >>  8))/255.0 \
+                         blue:((float)((rgbValue & 0x000000FF) >>  0))/255.0 \
+                        alpha:((float)((rgbValue & 0xFF000000) >> 24))/255.0]
+
+
 @interface Util : NSObject
 
 + (NSString *)errorMap:(const tABC_Error *)pError;
