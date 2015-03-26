9c46aaafe35b0dbc3406eca1fa30f20e81160155
tom <tom@airbitz.co>
2015-03-26 10:11:22 -0700

    added account switching and deletion

--- AirBitz/SlideoutView.m
+++ AirBitz/SlideoutView.m
@@ -7,18 +7,30 @@
 //
 
 #import "SlideoutView.h"
+#import "PickerTextView.h"
 #import "CoreBridge.h"
 #import "ABC.h"
 #import "User.h"
+#import "LocalSettings.h"
+#import "Util.h"
 
-@interface SlideoutView ()
+@interface SlideoutView () <PickerTextViewDelegate >
+
 {
-    CGRect   _originalSlideoutFrame;
-    BOOL     _open;
+    CGRect          _originalSlideoutFrame;
+    BOOL            _open;
+    NSString        *_account;
+    FadingAlertView                 *_fadingAlert;
 }
 
 @property (weak, nonatomic) IBOutlet UILabel                *conversionText;
 @property (weak, nonatomic) IBOutlet UILabel                *accountText;
+@property (weak, nonatomic) IBOutlet UIView                 *otherAccountsView;
+@property (weak, nonatomic) IBOutlet UIView                 *lowerViews;
+
+@property (nonatomic, strong) NSArray                       *arrayAccounts;
+@property (nonatomic, strong) NSArray                       *otherAccounts;
+@property (nonatomic, weak) IBOutlet PickerTextView         *accountPicker;
 
 @end
 
@@ -44,7 +56,7 @@
     f.origin.x = f.origin.x + f.size.width;
     v.frame = f;
     v->_open = NO;
-
+    
     return v;
 }
 
@@ -66,6 +78,16 @@
 
 - (void)showSlideout:(BOOL)show
 {
+    self.accountPicker.delegate = self;
+    
+    // set up the specifics on our picker text view
+    [self.accountPicker setTopMostView:self.otherAccountsView];
+    CGRect frame = self.accountPicker.frame;
+    frame.size.width = self.otherAccountsView.frame.size.width;
+    self.accountPicker.frame = frame;
+    self.accountPicker.pickerMaxChoicesVisible = 3;
+    [self.accountPicker setAccessoryImage:[UIImage imageNamed:@"btn_close.png"]];
+    
     tABC_AccountSettings *_pAccountSettings = NULL;
     tABC_Error Error;
     Error.code = ABC_CC_Ok;
@@ -75,7 +97,13 @@
                             &Error);
     NSInteger num = _pAccountSettings->currencyNum;
     self.conversionText.text = [CoreBridge conversionStringFromNum:num withAbbrev:NO];
-    self.accountText.text = @"account";
+    
+    
+    self.accountText.text = [User Singleton].name;
+    
+    self.lowerViews.hidden = NO;
+    self.otherAccountsView.hidden = YES;
+    
     [self showSlideout:show withAnimation:YES];
 }
 
@@ -137,9 +165,20 @@
 
 - (IBAction)accountTouched
 {
-    if (self.delegate && [self.delegate respondsToSelector:@selector(slideoutAccount)]) {
-        [self.delegate slideoutAccount];
+    if(self.otherAccountsView.hidden) {
+        [self updateOtherAccounts:self.accountText.text];
+        [self.accountPicker updateChoices:self.otherAccounts] ;
+        self.otherAccountsView.hidden = NO;
+        self.lowerViews.hidden = YES;
     }
+    else
+    {
+        self.otherAccountsView.hidden = YES;
+        self.lowerViews.hidden = NO;
+    }
+//    if (self.delegate && [self.delegate respondsToSelector:@selector(slideoutAccount)]) {
+//        [self.delegate slideoutAccount];
+//    }
 }
 
 - (IBAction)settingTouched
@@ -159,6 +198,126 @@
 - (BOOL)isOpen
 {
     return _open;
+}
+
+#pragma mark - PickerTextView delegates
+
+- (void)pickerTextViewPopupSelected:(PickerTextView *)pickerTextView onRow:(NSInteger)row
+{
+    [self.accountPicker dismissPopupPicker];
+    
+    // set the text field to the choice
+    NSString *account = [self.otherAccounts objectAtIndex:row];
+    [LocalSettings controller].cachedUsername = account;
+    if (self.delegate && [self.delegate respondsToSelector:@selector(slideoutLogout)]) {
+        [self.delegate slideoutLogout];
+    }
+}
+
+- (void)removeAccount:(NSString *)account
+{
+    tABC_Error error;
+    tABC_CC cc = ABC_AccountDelete((const char*)[account UTF8String], &error);
+    if(cc == ABC_CC_Ok) {
+        [self getAllAccounts];
+        [self.accountPicker updateChoices:self.arrayAccounts];
+    }
+    else {
+        [self showFadingError:[Util errorMap:&error]];
+    }
+}
+
+- (void)getAllAccounts
+{
+    char * pszUserNames;
+    tABC_Error error;
+    __block tABC_CC result = ABC_ListAccounts(&pszUserNames, &error);
+    switch (result)
+    {
+        case ABC_CC_Ok:
+        {
+            NSString *str = [NSString stringWithCString:pszUserNames encoding:NSUTF8StringEncoding];
+            NSArray *arrayAccounts = [str componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+            NSMutableArray *stringArray = [[NSMutableArray alloc] init];
+            for(NSString *str in arrayAccounts)
+            {
+                if(str && str.length!=0)
+                {
+                    [stringArray addObject:str];
+                }
+            }
+            self.arrayAccounts = [stringArray copy];
+            break;
+        }
+        default:
+        {
+            tABC_Error temp;
+            temp.code = result;
+            [self showFadingError:[Util errorMap:&temp]];
+            break;
+        }
+    }
+}
+
+- (void)updateOtherAccounts:(NSString *)username
+{
+    [self getAllAccounts];
+    NSMutableArray *stringArray = [[NSMutableArray alloc] init];
+    for(NSString *str in self.arrayAccounts)
+    {
+        if(![str isEqualToString:username])
+        {
+            [stringArray addObject:str];
+        }
+    }
+    self.otherAccounts = [stringArray copy];
+}
+
+
+- (void)pickerTextViewDidTouchAccessory:(PickerTextView *)pickerTextView categoryString:(NSString *)string
+{
+    _account = string;
+    NSString *message = [NSString stringWithFormat:@"Delete %@ on this device only?",
+                         string];
+    UIAlertView *alert = [[UIAlertView alloc]
+                          initWithTitle:NSLocalizedString(@"Delete Account", nil)
+                          message:NSLocalizedString(message, nil)
+                          delegate:self
+                          cancelButtonTitle:@"No"
+                          otherButtonTitles:@"Yes", nil];
+    [alert show];
+    [self.accountPicker dismissPopupPicker];
+}
+
+- (void)pickerTextViewFieldDidShowPopup:(PickerTextView *)pickerTextView
+{
+    CGRect popupWindowFrame = pickerTextView.popupPicker.frame;
+    
+    popupWindowFrame.size.width = pickerTextView.frame.size.width;
+    pickerTextView.popupPicker.frame = popupWindowFrame;
+}
+
+- (void)showFadingError:(NSString *)message
+{
+//    _fadingAlert = [FadingAlertView CreateInsideView:self.view withDelegate:self];
+//    _fadingAlert.message = message;
+//    _fadingAlert.fadeDuration = 2;
+//    _fadingAlert.fadeDelay = 5;
+//    [_fadingAlert blockModal:NO];
+//    [_fadingAlert showSpinner:NO];
+//    [_fadingAlert showFading];
+}
+
+#pragma mark - UIAlertView Delegate
+
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    // if they said they wanted to delete the account
+    if (buttonIndex == 1)
+    {
+        [self removeAccount:_account];
+        [self accountTouched];
+    }
 }
 
 @end
