ec4db83d844bfb09c4c8204e207bdbda9a368d9b
Tim Horton <tim@airbitz.co>
2015-04-15 12:34:32 -0700

    Upgrade plugin

--- AirBitz/Plugins/PluginViewController.m
+++ AirBitz/Plugins/PluginViewController.m
@@ -80,8 +80,6 @@ static const NSString *PROTOCOL = @"bridge://";
 
 - (void)viewWillAppear:(BOOL)animated
 {
-    [self resizeFrame:YES];
-
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
 
