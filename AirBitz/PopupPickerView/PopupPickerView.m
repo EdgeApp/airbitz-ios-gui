7b14698dce55b7234a96c0c85fa688789ba159e3
Paul Puey <paul@airbitz.co>
2015-05-21 01:50:29 -0700

    Add a background to Calculator

--- AirBitz/PopupPickerView/PopupPickerView.m
+++ AirBitz/PopupPickerView/PopupPickerView.m
@@ -9,6 +9,7 @@
 #import <QuartzCore/QuartzCore.h>
 #import "PopupPickerView.h"
 #import "MainViewController.h"
+#import "Theme.h"
 
 #define DEFAULT_WIDTH           330
 
@@ -718,7 +719,7 @@ CGRect keyboardFrame;
     cell.textLabel.font = [UIFont fontWithName:@"Lato-Regular" size:17.0];
     cell.textLabel.numberOfLines = 1;
     cell.textLabel.text = [_strings objectAtIndex:row];
-    cell.textLabel.textColor = [UIColor blackColor];
+    cell.textLabel.textColor = [Theme Singleton].colorTextDark;
     cell.accessoryView.hidden = YES;
     cell.backgroundColor = [UIColor clearColor];
     if(self.categories) {
