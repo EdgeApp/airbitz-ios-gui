2114cc89eb5d0ae86fd46f946b4f7aa8ec38e229
Paul Puey <paul@airbitz.co>
2015-10-08 15:15:11 -0700

    Copy widget code and enable. Woohoo it works

--- Airbitz Widget/TodayViewController.h
+++ Airbitz Widget/TodayViewController.h
@@ -2,8 +2,8 @@
 //  TodayViewController.h
 //  Airbitz Widget
 //
-//  Created by Paul P on 10/7/15.
-//  Copyright © 2015 Airbitz. All rights reserved.
+//  Created by Paul P on 8/31/15.
+//  Copyright (c) 2015 AirBitz. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
