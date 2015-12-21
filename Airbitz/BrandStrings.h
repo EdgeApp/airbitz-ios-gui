abd954061fef3dbbdeb6016f36fd63ca1118e4ee
Paul Puey <paul@airbitz.co>
2015-12-21 13:00:44 -0800

    Add white app logo to BrandStrings.h

--- Airbitz/BrandStrings.h
+++ Airbitz/BrandStrings.h
@@ -17,7 +17,8 @@
 #define supportEmail            @"support@coinbtm.com"
 #define appStoreLink            @"https://itunes.apple.com/us/app/airbitz/id843536046"
 #define playStoreLink           @"https://play.google.com/store/apps/details?id=com.airbitz"
-#define appLogoWhiteLink        @"https://airbitz.co/static/img/logo-nav.png"
+#define appDownloadLink         @"http://coinbtm.com/app"
+#define appLogoWhiteLink        @"https://airbitz.co/go/wp-content/uploads/2015/12/rack-logo-wht-100w.png"
 #define appDesignedBy           NSLocalizedString(@"Designed by CoinBTM in",nil)
 #define appCompanyLocation      NSLocalizedString(@"New York, New York, USA", nil)
 
