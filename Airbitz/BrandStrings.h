df91348e466a3ed6eee187abf2b99cd034033fef
Paul Puey <paul@airbitz.co>
2015-12-21 17:39:26 -0800

    Replace Appstore links with rack wallet URLs

--- Airbitz/BrandStrings.h
+++ Airbitz/BrandStrings.h
@@ -15,8 +15,8 @@
 #define appTitle                @"Rack Wallet"
 #define appHomepage             @"http://coinbtm.com"
 #define supportEmail            @"support@coinbtm.com"
-#define appStoreLink            @"https://itunes.apple.com/us/app/airbitz/id843536046"
-#define playStoreLink           @"https://play.google.com/store/apps/details?id=com.airbitz"
+#define appStoreLink            @"https://itunes.apple.com/us/app/rack-wallet/id1067132601"
+#define playStoreLink           @"https://play.google.com/store/apps/details?id=com.coinbtm.rackwallet"
 #define appDownloadLink         @"http://coinbtm.com/app"
 #define appLogoWhiteLink        @"https://airbitz.co/go/wp-content/uploads/2015/12/rack-logo-wht-100w.png"
 #define appDesignedBy           NSLocalizedString(@"Designed by CoinBTM in",nil)
