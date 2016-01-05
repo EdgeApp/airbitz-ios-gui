ccf2e921b0ccf96f343a240ef3e0550745e66877
Lucas Betschart <lucasbetschart@gmail.com>
2016-01-05 19:33:43 +0100

    Exchange Rate Sources: Add CleverCoin

--- Airbitz/Settings/SettingsViewController.m
+++ Airbitz/Settings/SettingsViewController.m
@@ -77,7 +77,7 @@
 #define ROW_PIN_RELOGIN                 7
 #define ROW_TOUCHID                     8
 
-#define ARRAY_EXCHANGES     @[@"Bitstamp", @"BraveNewCoin", @"Coinbase"]
+#define ARRAY_EXCHANGES     @[@"Bitstamp", @"BraveNewCoin", @"Coinbase", @"CleverCoin"]
 
 #define ARRAY_LOGOUT        @[@[@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9", \
                                 @"10",@"11",@"12",@"13",@"14",@"15",@"16",@"17",@"18",@"19", \
