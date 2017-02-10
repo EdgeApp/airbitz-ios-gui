d3f259c7ed7327b6a3c6ccc899dc59df9b9e6465
Paul Puey <paul@airbitz.co>
2017-02-09 23:59:24 -0800

    Enable Rockitcoin but without proper assets

--- Airbitz/BrandTheme.h
+++ Airbitz/BrandTheme.h
@@ -135,6 +135,37 @@
 #define LOCKED_SEARCH_CATEGORY                          1
 #define LOCKED_SEARCH_CATEGORY_STRING                   @"ATM"
 
+#elif ROCKITCOIN
+
+#define AppFont                                         @"Lato-Regular"
+#define AppFontItalic                                   @"Lato-Italic"
+
+#define ColorOffBright                                  UIColorFromARGB(0xffeeeeee)
+#define ColorOffBrightFrost                             UIColorFromARGB(0xaaeeeeee)
+#define ColorDarkGrey                                   UIColorFromARGB(0xff383838)
+#define ColorWhiteFrost                                 UIColorFromARGB(0xaaffffff)
+#define ColorDarkBlue                                   UIColorFromARGB(0xff0E4379)
+#define ColorLightBlue                                  UIColorFromARGB(0xffCFEDF6)
+
+#define ColorPinUserNameSelectorShadow                  UIColorFromARGB(0xff3756B8)
+#define ColorPinEntryText                               ColorOffBright
+#define ColorPinEntryUsernameText                       ColorLightBlue
+
+#define ColorLoginTitleText                             ColorOffBright
+#define ColorLoginTitleTextShadow                       ColorOffBrightFrost
+
+#define DirectoryCategoryButtonsBackgroundColor         ColorWhiteFrost
+
+#define LoginTitleTextShadowRadius                      0.5f
+#define PinEntryTextShadowRadius                        0.5f
+
+#define DefaultBTCDenominationMultiplier                ABCDenominationMultiplierBTC
+
+#define SHOW_BUY_SELL                                   0
+#define SHOW_AFFILIATE                                  0
+#define LOCKED_SEARCH_CATEGORY                          1
+#define LOCKED_SEARCH_CATEGORY_STRING                   @"ATM"
+
 #endif
 
 #endif /* BrandTheme_h */
