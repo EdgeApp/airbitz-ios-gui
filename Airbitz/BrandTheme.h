d2deb489579f4106ca82437ad27b70138d5e93d7
Paul Puey <paul@airbitz.co>
2017-05-12 17:11:43 -0700

    Hide promo rows for Coinsource

--- Airbitz/BrandTheme.h
+++ Airbitz/BrandTheme.h
@@ -132,6 +132,7 @@
 
 #define SHOW_BUY_SELL                                   0
 #define SHOW_AFFILIATE                                  0
+#define HIDE_PROMO_ROWS                                 1
 #define LOCKED_SEARCH_CATEGORY                          1
 #define LOCKED_SEARCH_CATEGORY_STRING                   @"ATM"
 
